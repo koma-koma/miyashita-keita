112b810fba81f6325f7dd02c8653af5ca8c0ff9c
koma-koma <keikei12@gmail.com>
2017-06-16 18:07:27 +0900

    update page transition

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,27 @@
+// $(function(){
+//   $('a[href^=#]').click(function(){
+//     var speed = 800;
+//     var href= $(this).attr("href");
+//     var target = $(href == "#" || href == "" ? 'html' : href);
+//     var position = target.offset().top - 60;
+//     $("html, body").animate({scrollTop:position}, speed, "swing");
+//     $('.menu > a').removeClass('here');
+//     $(this).addClass('here');
+//     return false;
+//   });
+// });
+
 $(function(){
   $('a[href^=#]').click(function(){
-    var speed = 800;
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top - 60;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
+
     $('.menu > a').removeClass('here');
     $(this).addClass('here');
+
+    $('main > div[id!=koma]').addClass('invisible');
+    $('main > div[class*=now]').removeClass('fadeIn now').fadeOut();
+    $(href).removeClass('invisible').removeClass('fadeOut').addClass('now').fadeIn();
     return false;
   });
 });
