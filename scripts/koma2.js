9618b4936e65ae0117a9180080f0ab563ceab6ce
koma-koma <keikei12@gmail.com>
2017-06-23 15:02:50 +0900

    update koma

--- scripts/koma2.js
+++ scripts/koma2.js
@@ -8,7 +8,7 @@ function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("koma");
   colorMode(HSB, 255);
-  rectSize = 50;
+  rectSize = 80;
   // colorMode(RGB, 255, 255, 255, 255);
 
   // blendMode(ALPHA);
@@ -16,22 +16,33 @@ function setup() {
 }
 
 function draw() {
-  easingx = easingx + (mouseX - easingx)*0.03;
-  easingy = easingy + (mouseY - easingy)*0.03;
+  easingx = easingx + (mouseX - easingx)*0.05;
+  easingy = easingy + (mouseY - easingy)*0.05;
 
   noStroke();
-  fill(230, 0, 255);
+  fill(142, 12, 230, 255);
   rect(0, 0, width, height);
 
-  for (var y = 0; y <= height/rectSize + 1; y++) {
-    for (var x = 0; x <= width/rectSize + 1; x++) {
+  var nrectx = width/rectSize+1;
+  var nrecty = height/rectSize+1;
+
+  for (var y = 0; y <= nrecty; y++) {
+    for (var x = 0; x <= nrectx; x++) {
       var zindex = dist(easingx, easingy, x*rectSize, y*rectSize);
       var maxdist = dist(0, 0, windowWidth, windowHeight);
       zindex = map(zindex, 0, maxdist, 80, 0);
 
       push();
       translate(0, 0, 0);
-      fill(100+x*2, 64, 255, 100);
+      // fill(100+x*2, 64, 255, 100);
+      // if (x == int((mouseX+rectSize/2)/rectSize) &&
+      //     y == int((mouseY+rectSize/2)/rectSize)) {
+      //   fill(84, 180, 160, 100);
+      // } else {
+      //   fill(0, 0, 255, 100);
+      // }
+
+      fill(0, 255*(zindex-60)/40, 200, 100);
       rectMode(CENTER);
       rect(x*rectSize, y*rectSize, rectSize*(zindex/100), rectSize*(zindex/100));
      pop();
