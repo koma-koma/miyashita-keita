b41decfe90a5f1fe0ef3abe5032639ec91418790
koma-koma <keikei12@gmail.com>
2017-06-29 14:34:23 +0900

    fix

--- scripts/koma2.js
+++ scripts/koma2.js
@@ -1,14 +1,32 @@
 var rectSize;
+var nrectx
+var nrecty
+
 var canvas;
 
 var easingx = 0;
 var easingy = 0;
 
+var array = [];
+var noisex = [];
+var noisey = [];
+var startnoisex = 0;
+var startnoisey = 0;
+
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent("koma");
+  canvas.parent("koma2");
   colorMode(HSB, 255);
-  rectSize = 80;
+  nrectx = 25;
+  rectSize = width/(nrectx-1);
+  nrecty = height/rectSize +1;
+
+  for (var i = 0; i < nrectx*nrecty; i++) {
+    array.push(Math.random()*255);
+    noisex.push(Math.random() + i/100);
+    noisey.push(i/100);
+  }
+
   // colorMode(RGB, 255, 255, 255, 255);
 
   // blendMode(ALPHA);
@@ -23,11 +41,12 @@ function draw() {
   fill(142, 12, 230, 255);
   rect(0, 0, width, height);
 
-  var nrectx = width/rectSize+1;
-  var nrecty = height/rectSize+1;
+  for (var y = 0; y < nrecty; y++) {
+    // noisey = startnoisey;
 
-  for (var y = 0; y <= nrecty; y++) {
-    for (var x = 0; x <= nrectx; x++) {
+    for (var x = 0; x < nrectx; x++) {
+      // noisex = startnoisex;
+
       var zindex = dist(easingx, easingy, x*rectSize, y*rectSize);
       var maxdist = dist(0, 0, windowWidth, windowHeight);
       zindex = map(zindex, 0, maxdist, 80, 0);
@@ -42,14 +61,24 @@ function draw() {
       //   fill(0, 0, 255, 100);
       // }
 
-      fill(0, 255*(zindex-60)/40, 200, 100);
+      fill(0, 255*(zindex-60)/40, array[x + y*nrectx], 100);
+      // fill(0, 255*(zindex-60)/40, noise(noisex[x + y*nrectx], noisey[x + y*nrectx])*255, 100);
+
       rectMode(CENTER);
-      rect(x*rectSize, y*rectSize, rectSize*(zindex/100), rectSize*(zindex/100));
+      var size = noise(noisex[x + y*nrectx], noisey[x + y*nrectx])*10;
+      rect(x*rectSize, y*rectSize,
+        rectSize*(zindex/100)+size,
+        rectSize*(zindex/100)+size);
      pop();
+     noisex[x + y*nrectx] += 0.01;
     }
+  noisey[x + y*nrectx] += 0.01;
+
   }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  rectSize = width/(nrectx-1);
+  nrecty = height/rectSize +1;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,11 @@
-// $(function(){
-//   $('a[href^=#]').click(function(){
-//     var speed = 800;
-//     var href= $(this).attr("href");
-//     var target = $(href == "#" || href == "" ? 'html' : href);
-//     var position = target.offset().top - 60;
-//     $("html, body").animate({scrollTop:position}, speed, "swing");
-//     $('.menu > a').removeClass('here');
-//     $(this).addClass('here');
-//     return false;
-//   });
-// });
-
-
 
 $(function() {
-  console.log("test");
   var hash = location.hash;
 
   $('.menu > a').removeClass('here');
   $(this).addClass('here');
 
-  $('main > div[id!=koma]').addClass('invisible');
+  $('main > div[class!=back]').addClass('invisible');
   $('main > div[class*=now]').removeClass('fadeIn now').fadeOut();
   $(hash).removeClass('invisible').removeClass('fadeOut').addClass('now').fadeIn();
 
@@ -33,34 +18,4 @@ $(function() {
     $('main > div[class*=now]').removeClass('fadeIn now').fadeOut();
     $(href).removeClass('invisible').removeClass('fadeOut').addClass('now').fadeIn();
   });
-
 });
-
-
-
-// $(function() {
-//
-//     var urlHash = location.hash;
-//     if (urlHash) {
-//       alert.('test');
-//
-//       $('.menu > a').removeClass('here');
-//       $(this).addClass('here');
-//
-//       $('main > div[id!=koma]').addClass('invisible');
-//       $('main > div[class*=now]').removeClass('fadeIn now').fadeOut();
-//       $(urlHash).removeClass('invisible').removeClass('fadeOut').addClass('now').fadeIn();
-//     }
-//
-// });
-
-// スクロール値でメニューの色変えたい
-// $(function() {
-//   var
-//   var href= $(this).attr("href");
-//   var target = $(href == "#" || href == "" ? 'html' : href);
-//   var position = target.offset().top;
-//   $("html, body").animate({scrollTop:position}, speed, "swing");
-//   $('.menu > a').removeClass('here');
-//   $(this).addClass('here');
-// })
